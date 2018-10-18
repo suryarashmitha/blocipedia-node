4b3f0afe2aa6c71adde4c7ad14ab4e2e7a011d01
suryarashmitha <rashmitta.c@gmail.com>
2018-10-18 01:18:42 -0500

    Checkpoint-seedingdata: completed

---
+++ src/db/seeds/20181018055721-user.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const faker = require("faker");
+
+let users = [];
+
+for(let i=1; i<=15; i++){
+  users.push({
+    username: faker.hacker.noun(),
+    email: faker.internet.email(),
+    password: faker.hacker.noun(),
+    role: "standard",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  });
+}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Users", users, {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Users", null, {});
+  }
+};
